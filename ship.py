6be8ca1bd5939398e3544ef076d3fc49af5d3575
Ian Suto <sutomusima@gmail.com>
2025-01-06 09:55:06 +0300

    Prevent ship from moving outside screen

--- ship.py
+++ ship.py
@@ -5,6 +5,7 @@ class Ship:
     def __init__(self, ai_game):
         """Intialize the ship and set its starting position."""
         self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
         self.settings = ai_game.settings
 
         #Load the ship image and get its rect.
